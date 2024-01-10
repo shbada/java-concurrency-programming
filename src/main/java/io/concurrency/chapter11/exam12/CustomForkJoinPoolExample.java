bf622297d382102d9148b82c6d28f3e1adb2d112
seohaebada <seohae0406@gmail.com>
2024-01-10 20:14:47 +0900

    (01.10) ForkJoinPool example

--- src/main/java/io/concurrency/chapter11/exam12/CustomForkJoinPoolExample.java
+++ src/main/java/io/concurrency/chapter11/exam12/CustomForkJoinPoolExample.java
@@ -8,10 +8,31 @@ public class CustomForkJoinPoolExample {
 
         int[] array = new int[10];
         for (int i = 0; i < array.length; i++) {
+            // [0,1,2,3,4,5,6,7,8,9]
+            // [0,1] = 0
+            // [1,2] = 1 = 0(위에 [0,1]) + 1 = 1
+            // [2,3] = 2
+            // [3,4] = 3 = 2(위에 [2,3]) + 3 = 5 => 5 + 1(위에 [0,1][1,2]) = 6
+            // [4,5] = 4
+            // [5,6] = 5 = 4 + 5 = 9
+            // [6,7] = 6
+            // [7,8] = 7
+            // [8,9] = 8
+
             array[i] = i;
         }
+
+        // ForkJoinPool 생성 (쓰레드수 = CPU 코어 갯수만큼)
         ForkJoinPool pool = new ForkJoinPool(Runtime.getRuntime().availableProcessors());
 
+        // RecursiveTask 상속받는 커스텀 클래스 생성
+        // compute() 오버라이딩
+        /*
+           RecursiveTask
+           n 이 1보다 크면 Fibonacci 클래스 f1, f2 생성해서 두개의 클래스를 가지고 compute 작업을 수행
+           (1개는 fork(), 다른 하나는 직접 compute() 수행하고 f1을 join()해서 결과를 기다림)
+           그러고 합산해서 결과 다시 compute() 수행
+         */
         CustomRecursiveTask task = new CustomRecursiveTask(array, 0, array.length);
         long result = pool.invoke(task);
 
--- src/main/java/io/concurrency/chapter11/exam12/CustomRecursiveTask.java
+++ src/main/java/io/concurrency/chapter11/exam12/CustomRecursiveTask.java
@@ -15,7 +15,7 @@ public class CustomRecursiveTask extends RecursiveTask<Long> {
     }
 
     @Override
-    protected Long compute() {
+    protected Long compute() { // 자체가 queue에 들어가는 작업
         if (end - start < THRESHOLD) {
             long sum = 0;
             for (int i = start; i < end; i++) {
@@ -24,14 +24,18 @@ public class CustomRecursiveTask extends RecursiveTask<Long> {
 
             return sum;
         } else {
-            int mid = start + (end - start) / 2;
+            int mid = start + (end - start) / 2; // 중간값
+            // task 분할
             CustomRecursiveTask left = new CustomRecursiveTask(array, start, mid);
             CustomRecursiveTask right = new CustomRecursiveTask(array, mid, end);
 
-            left.fork();
-            long rightResult = right.compute();
-            long leftResult = left.join();
+            // fork() 수행하면, 별도의 쓰레드가 이 task의 compute()를 호출하게된다.
+            left.fork(); // 해당 task가 forkJoinPool의 queue에 들어감 (쓰레드가 이 queue에서 꺼내서 수행한다는 의미)
+            // 직접 compute() 한다는건 현재 쓰레드가 작업을 수행한다는 것이다.
+            long rightResult = right.compute(); // 재귀
+            long leftResult = left.join(); // 작업을 마치고 올때까지 기다린다.
 
+            // right는 현재 쓰레드가 수행하므로 바로 가능 
             return leftResult + rightResult;
         }
     }
