1b95543689d26498061d39bb16e34f7025b06176
seohaebada <seohae0406@gmail.com>
2024-01-13 13:15:27 +0900

    (01.13) name() / currentThread() / isAlive()

--- src/main/java/io/concurrency/chapter03/exam04/CurrentThreadExample.java
+++ src/main/java/io/concurrency/chapter03/exam04/CurrentThreadExample.java
@@ -4,7 +4,7 @@ public class CurrentThreadExample {
     public static void main(String[] args) {
 
         // 현재 실행 중인 스레드를 가져와 이름 출력
-        System.out.println("현재 스레드: " + Thread.currentThread());
+        System.out.println("현재 스레드: " + Thread.currentThread()); // native method
         System.out.println("현재 스레드 이름: " + Thread.currentThread().getName());
 
         // Thread 객체를 사용하여 스레드 생성 및 시작
