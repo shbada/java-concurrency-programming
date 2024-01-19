de85bd8bc4321d54543094592b4765133cfd01a8
seohaebada <seohae0406@gmail.com>
2024-01-19 19:29:31 +0900

    (01.19) Monitor - 모니터 -1,2

--- src/main/java/io/concurrency/chapter06/exam03/ConditionSynchronizationExample.java
+++ src/main/java/io/concurrency/chapter06/exam03/ConditionSynchronizationExample.java
@@ -1,19 +1,21 @@
 package io.concurrency.chapter06.exam03;
 
 public class ConditionSynchronizationExample {
-    private boolean isAvailable = false;
+    private boolean isAvailable = false; // 조건변수
 
     public synchronized void produce() {
         while (isAvailable) {
             try {
+                // 다른 스레드가 들어오더라도 무조건 대기
                 wait();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
         System.out.println("생산됨.");
+        // 깨워지면 수행
         isAvailable = true;
-        notify();
+        notify(); // consume() 호출 쪽에 스레드가 있다면 깨워준다.
     }
 
     public synchronized void consume() {
@@ -26,7 +28,7 @@ public class ConditionSynchronizationExample {
         }
         System.out.println("소비됨.");
         isAvailable = false;
-        notify();
+        notify(); // produce() 호출쪽에 스레드가 있다면 깨워준다.
     }
 
     public static void main(String[] args) {
--- src/main/java/io/concurrency/chapter06/exam03/MutualExclusionExample.java
+++ src/main/java/io/concurrency/chapter06/exam03/MutualExclusionExample.java
@@ -3,7 +3,9 @@ package io.concurrency.chapter06.exam03;
 public class MutualExclusionExample {
     private int counter = 0;
 
+    // synchronized : 내부적으로 모니터 기능을 가지고있다.
     public synchronized void increment() {
+        // 두 스레드가 동시에 호출할때 아래 공유변수를 동기화블록 안에서 처리해야한다. (상호배제의 기능이 들어가야한다.)
         counter++;
         System.out.println("스레드: " + Thread.currentThread().getName() + ", 카운터 값: " + counter);
     }
