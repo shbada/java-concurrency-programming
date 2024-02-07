92df164cf231c7d67b2840c441481d222b99e66d
seohaebada <seohae0406@gmail.com>
2024-02-07 19:17:34 +0900

    (02.07) ReentrantReadWriteLock 재 진입성

--- src/main/java/io/concurrency/chapter08/exam05/ReentrantReadWriteLockDowngradeExample.java
+++ src/main/java/io/concurrency/chapter08/exam05/ReentrantReadWriteLockDowngradeExample.java
@@ -15,18 +15,25 @@ public class ReentrantReadWriteLockDowngradeExample {
         // 여러 개의 스레드가 쓰기 락을 사용하는 경우
         for (int i = 0; i < 1; i++) {
             new Thread(() -> {
+                // 쓰기락 획득 (상호배제)
                 writeLock.lock();
+
                 try {
                     System.out.println("쓰기 락 획득: " + Thread.currentThread().getName());
 
-                    // 임계 영역 수행
+                    // 임계 영역 수행 (상호배제)
                     sharedData.setData((int) (Math.random() * 100));
                     System.out.println("데이터 업데이트");
 
-                    // 쓰기 락을 읽기 락으로 다운그레이드
+                    // 쓰기 락을 읽기 락으로 다운그레이드 (락의 재진입)
+                    // write lock -> read lock
+                    // 재진입 가능
                     readLock.lock();
                     System.out.println("다운그레이드: 쓰기 락 -> 읽기 락");
+
                     // 쓰기 락 해제
+                    // 이 전까지의 영역만 상호배제
+                    // 이 이후부터는 read lock만 가지고 있는 상태가 됨 -> 여러 스레드 접근 가능
                     writeLock.unlock();
                     System.out.println("쓰기 락 해제: " + Thread.currentThread().getName());
 
@@ -58,7 +65,7 @@ public class ReentrantReadWriteLockDowngradeExample {
         }
     }
     static class SharedData {
-        private int data = 0;
+        private int data = 0; // 공유변수
 
         public int getData() {
             return data;
--- src/main/java/io/concurrency/chapter08/exam05/ReentrantReadWriteLockUpgradeExample.java
+++ src/main/java/io/concurrency/chapter08/exam05/ReentrantReadWriteLockUpgradeExample.java
@@ -8,11 +8,13 @@ public class ReentrantReadWriteLockUpgradeExample {
 
     public void failedUpgradeAttempt() {
         System.out.println("읽기 잠금 획득 시도...");
+        // 읽기락 획득
         lock.readLock().lock();
         System.out.println("읽기 잠금 획득!");
 
         try {
             System.out.println("쓰기 잠금 획득 시도...");
+            // 쓰기락 획득 불가
             lock.writeLock().lock(); // 데드락 발생
             System.out.println("이 메시지는 출력되지 않습니다.");
         } finally {
