b111742ede99286104780bf24d033171552d292e
seohaebada <seohae0406@gmail.com>
2024-02-12 10:46:57 +0900

    (02.12) CyclicBarrier

--- src/main/java/io/concurrency/chapter09/exam04/CyclicBarrierExample.java
+++ src/main/java/io/concurrency/chapter09/exam04/CyclicBarrierExample.java
@@ -8,9 +8,12 @@ public class CyclicBarrierExample {
 
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
         int numThreads = 2;
+        // 장벽 액션 : new BarrierAction(parallelSum)
         CyclicBarrier barrier = new CyclicBarrier(numThreads, new BarrierAction(parallelSum));
 
+        // 쓰레드 2개
         for (int i = 0; i < numThreads; i++) {
             new Thread(new Worker(i, numbers, barrier, parallelSum)).start();
         }
@@ -24,8 +27,14 @@ class BarrierAction implements Runnable {
         this.parallelSum = parallelSum;
     }
 
+    /**
+     * 모든 스레드가 장벽에 모인 시점에 실행된다.
+     */
+    @Override
     public void run() {
         int finalSum = 0;
+
+        // 최종 합산
         for (int sum : parallelSum) {
             finalSum += sum;
         }
@@ -47,16 +56,26 @@ class Worker implements Runnable {
     }
 
     public void run() {
+        // 첫번째 쓰레드 : 0 ~ 4
+        // 두번째 쓰레드 : 5 ~ 9
         int start = id * (numbers.length / 2);
         int end = (id + 1) * (numbers.length / 2);
         int sum = 0;
+
         for (int i = start; i < end; i++) {
             sum += numbers[i];
         }
-        parallelSum[id] = sum;
+
+        // id = 0, 1
+        parallelSum[id] = sum; // 각 스레드는 병렬로 데이터를 나누어 연산한다.
 
         try {
-            barrier.await();
+            // 총 쓰레드 2개가 모두 await()을 할때까지 대기
+            barrier.await(); // 모든 스레드가 합산을 완료할 때까지 대기한다.
+
+            // 여기서 다시 barrier.await() 할 수 있음 (재사용 가능)
+            // 다시 2개의 쓰레드가 모두 await()을 호출 할때까지 기다린다.
+//            barrier.await(); // 모든 스레드가 합산을 완료할 때까지 대기한다.
         } catch (InterruptedException | BrokenBarrierException e) {
             e.printStackTrace();
         }
